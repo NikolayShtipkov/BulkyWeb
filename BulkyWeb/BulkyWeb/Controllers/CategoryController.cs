bb7e1ab27f02ba6b0e52b76fd75e5a52c2b562ce
Nikolay Shtipkov <nikolayshtipkov@gmail.com>
2024-05-01 14:21:06 +0300

    add edit and delete action methods and views

--- BulkyWeb/BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/BulkyWeb/Controllers/CategoryController.cs
@@ -49,5 +49,67 @@ namespace BulkyWeb.Controllers
 
             return View();
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            // Can use first, single, find, last or the defaults
+            var category = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var category = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Category? category = _db.Categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            _db.Categories.Remove(category);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
     }
 }
